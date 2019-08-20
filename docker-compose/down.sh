bdb96cc4a92005fb779cb13ad9ea413cc6cd03e4
sevendollar <sevendollar@gmail.com>
2019-08-20 09:35:32 +0000

    add up & down script

---
+++ docker-compose/down.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# 1st step
+docker-compose down -v --remove-orphans
---
+++ docker-compose/up.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 1st step
+docker-compose -f create-certs.yml run --rm create_certs && \
+
+# 2nd step
+docker-compose up
